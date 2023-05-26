0e9f8f836350dbc40406def148a942ab5a572bdf
suongfiori <suong.kf@gmail.com>
2023-05-26 13:04:36 +0100

    added code break down explanation

--- two-sum.js
+++ two-sum.js
@@ -35,3 +35,31 @@ function twoSum(nums, target) {
 
 console.log(twoSum([2, 7, 11, 15], 9))
   
+// Code break down
+
+/* 
+Given the input nums = [2, 7, 11, 15] and target = 9:
+
+Step 1: Initialize an empty numMap object: numMap = {}
+
+Step 2: Start iterating through the nums array.
+    For the first iteration (nums[0] = 2), calculate the complement (complement = target - nums[0] = 9 - 2 = 7).
+    complement = 7
+
+Step 3: Check if the complement exists in numMap.
+    Since numMap is empty, the complement (7) is not found.
+
+Step 5: Store the current number (2) and its index (0) in numMap.
+    numMap = {
+    2: 0
+    }
+    For the second iteration (nums[1] = 7), calculate the complement (complement = target - nums[1] = 9 - 7 = 2).
+    complement = 2
+
+Step 3: Check if the complement exists in numMap.
+    The complement (2) is found in numMap, and its corresponding index is 0.
+
+Step 4: Return the indices [0, 1] as the pair [nums[0], nums[1]] = [2, 7] adds up to the target (9).
+    The final result is [0, 1], which represents the indices of the two numbers that add up to the target.
+
+*/
