88933bb84a32b0bf9b8bb3900a54aaf0be63b2a7
suongfiori <suong.kf@gmail.com>
2023-06-05 15:57:10 +0100

    "Updated approach for handling valid parentheses"

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -36,7 +36,15 @@ function isValid(s) {
   return stack.length===0 // Return true if the stack is empty (all brackets matched), false otherwise
 }
 
+console.log(isValid("()[]{}")) //  Output: true
+console.log(isValid("(]")) //  Output: false
 
+// Approach:
 
-console.log(isValid("()[]{}"))
-console.log(isValid("(]"))
+/* 
+- Using a stack data structure implemented with an array to efficiently check and remove matching bracket pairs.
+- Iterating through each character in the string, pushing matching closing brackets onto the stack 
+  for opening brackets and checking for matches and removals for closing brackets.
+- The time and space complexity of the code is linear, 
+  as it iterates through each character in the string and the space used by the stack grows linearly with the size of the string.
+*/
